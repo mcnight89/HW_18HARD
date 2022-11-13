7e3063c41ec19d04b255165649e5ce484be3dc2c
Pavel Tokarev <108064386+mcnight89@users.noreply.github.com>
2022-11-13 14:26:29 +0300

    Add files via upload

---
+++ app/config.py
@@ -0,0 +1,5 @@
+class Config(object):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///movies.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    RESTX_JSON = {'ensure_ascii': False, 'indent': 2}
